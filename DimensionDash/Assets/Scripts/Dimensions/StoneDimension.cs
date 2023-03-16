554d5622a4a0a27adb83ffe912c7c735c6e7ee2d
Clinc8686 <mario1.lanpert@gmail.com>
2023-03-16 11:15:58 +0100

    Added stone dimension without tile set

---
+++ DimensionDash/Assets/Scripts/Dimensions/StoneDimension.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using Player;
+using UnityEngine;
+
+namespace Dimensions
+{
+	[CreateAssetMenu(fileName = "Dimension", menuName = "Dimensions/StoneDimension", order = 0)]
+	public class StoneDimension : DimensionDescription
+	{
+		public override void Apply(List<GameObject> players)
+		{
+			foreach(GameObject player in players)
+			{
+				player.GetComponent<BewegenPlatformToolkit>().enabled = false;
+			}
+		}
+
+		public override void UnApply(List<GameObject> players)
+		{
+			foreach(GameObject player in players)
+			{
+				player.GetComponent<BewegenPlatformToolkit>().enabled = true;
+			}
+		}
+	}
+}
