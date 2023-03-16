c91226fd7cc14c4ba18ea4007fff4ef05afae1c6
Florian Oetke <lowkey@second-system.de>
2023-03-16 15:52:00 +0100

    added space, stone and arcade dimension tilesets

---
+++ DimensionDash/Assets/Scripts/Dimensions/FlipperDimension.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Dimensions {
+	[CreateAssetMenu(fileName = "Dimension", menuName = "Dimensions/FlipperDimension", order = 0)]
+	public class FlipperDimension : DimensionDescription {
+
+		public override void Apply(List<GameObject> players) {
+		}
+
+		public override void UnApply(List<GameObject> players) {
+		}
+	}
+}
