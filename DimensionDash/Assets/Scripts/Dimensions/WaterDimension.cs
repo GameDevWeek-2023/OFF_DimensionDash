fd438be33020ef74eb18c0059968ab625715ae64
Florian Oetke <lowkey@second-system.de>
2023-03-16 16:03:43 +0100

    integrated underwater tileset

---
+++ DimensionDash/Assets/Scripts/Dimensions/WaterDimension.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Dimensions {
+	[CreateAssetMenu(fileName = "Dimension", menuName = "Dimensions/WaterDimension", order = 0)]
+	public class WaterDimension : DimensionDescription {
+
+		public override void Apply(List<GameObject> players) {
+		}
+
+		public override void UnApply(List<GameObject> players) {
+		}
+	}
+}
