5f79d20bd181228c3cb662a69543125862184c79
Jonas Hoffmann <isajohoff@gmail.com>
2023-03-18 17:46:21 +0100

    added CoinPickupFX

--- DimensionDash/Assets/Scripts/Player/PlayerPoints.cs
+++ DimensionDash/Assets/Scripts/Player/PlayerPoints.cs
@@ -6,12 +6,17 @@ namespace Player {
 		[SerializeField] private int _points_per_coin = 20;
 		
 		public int points = 0;
+		public GameObject coinFX;
 
 		private void OnTriggerEnter2D(Collider2D col)
 		{
 			if (col.gameObject.tag == "Item")
 			{
 				updatePlayerPoints(_points_per_coin);
+
+				GameObject fx = Instantiate(coinFX);
+				fx.transform.position = col.transform.position;
+
 				Destroy(col.gameObject);
 			}
 		}
